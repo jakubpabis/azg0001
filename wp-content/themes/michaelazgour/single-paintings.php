22054eb54623d2173c180a1b0e341404edd1d784
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-12-09 13:11:08 +0100

    asdsad

--- wp-content/themes/michaelazgour/single-paintings.php
+++ wp-content/themes/michaelazgour/single-paintings.php
@@ -13,7 +13,7 @@
 								<p>Share this painting:</p>
 								<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_the_permalink(); ?>" class="facebook" title="share on Facebook"><i class="fa fa-facebook"></i></a>
 								<a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_the_permalink(); ?>&title=<?php echo get_the_title(); ?>" class="linkedin" title="share on Linkedin"><i class="fa fa-linkedin"></i></a>
-								<a target="_blank" href="https://twitter.com/intent/tweet?url<?php echo get_the_permalink(); ?>&text=<?php echo get_the_title(); ?>" class="twitter" title="share on Twitter"><i class="fa fa-twitter"></i></a>
+								<a target="_blank" href="https://twitter.com/intent/tweet?url=<?php echo get_the_permalink(); ?>&text=<?php echo get_the_title(); ?>" class="twitter" title="share on Twitter"><i class="fa fa-twitter"></i></a>
 								<a target="_blank" href="mailto:?subject=<?php the_title(); ?>&body=<?php echo get_the_permalink(); ?>" class="mail" title="share via e-mail"><i class="fa fa-envelope"></i></a>
 							</div>
 						</div>
