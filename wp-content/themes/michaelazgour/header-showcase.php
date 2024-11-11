a7f8d42607d85f0755df7199e92eed472decc85b
jakubpabis <pabis91@gmail.com>
2024-11-11 21:23:36 +0100

    version

--- wp-content/themes/michaelazgour/header-showcase.php
+++ wp-content/themes/michaelazgour/header-showcase.php
@@ -21,7 +21,7 @@
 		document.cookie = 'resolution=' + Math.max(screen.width, screen.height) + '; path=/';
 	</script>
 	<link href='https://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri()); ?>/sass/style.min.css?v=1.47">
+	<link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri()); ?>/sass/style.min.css?v=1.48">
 	<script src="https://use.fontawesome.com/2c188634d9.js" async defer></script>
 	<?php wp_head(); ?>
 	<script id="mcjs">
